43bca532de5be90646cb8f49c14b57acbb7081fd
Boz <ryanmturnbull@gmail.com>
2022-07-22 21:12:07 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
+
+	RegisterKeyMapping('e c', 'Cancel current emote', 'keyboard', '')
 Config = {
     -- Change the language of the menu here!.
     -- Note fr and de are google translated, if you would like to help out with translations, feel free to send me an 'issue' on Github.
@@ -8,7 +10,7 @@ Config = {
     -- Set this to true to enable some extra prints
     DebugDisplay = false,
     -- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
-    EnableXtoCancel = true,
+    EnableXtoCancel = false,
     -- Set this to true if you want to disarm the player when they play an emote.
     DisarmPlayer = false,
     -- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
